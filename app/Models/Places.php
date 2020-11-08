b5a44601d2d45b93980d0eae377232a7b1b6024d
ushioChan <1072085644@qq.com>
2020-11-08 18:11:02 +0800

    11/8

--- app/Models/Places.php
+++ app/Models/Places.php
@@ -1,12 +1,16 @@
 <?php
-namespace App\Models;
 
+namespace App\Models;
+use App\Models\Simple\XMLModel;
 /*
  * Mock travel destination data.
  * Note that we don't have to extend CodeIgniter's model for now
  */
 
 class Places {
+protected $origin = WRITEPATH . 'data/placesData.xml';
+protected $keyField = 'id';
+protected $validationRules = [];
 
     //mock data : an array of records
     protected $data = [
